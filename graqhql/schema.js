9efbff0fdaeda222543ee2907e7d933cd9e2b184
Pokem <pokempatrick@yahoo.fr>
2023-04-20 10:38:56 +0100

    usage of makeExecutableSchema instead of buildSchema

--- graqhql/schema.js
+++ graqhql/schema.js
@@ -2,7 +2,11 @@ const path = require("path");
 const { buildSchema, print } = require("graphql");
 const { mergeTypeDefs } = require("@graphql-tools/merge");
 const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typesArray = loadFilesSync(path.join(__dirname, "./schemas"));
 
-module.exports = buildSchema(print(mergeTypeDefs(typesArray)));
+module.exports = makeExecutableSchema({
+    typeDefs: print(mergeTypeDefs(typesArray)),
+});
+// module.exports = buildSchema(print(mergeTypeDefs(typesArray)));
