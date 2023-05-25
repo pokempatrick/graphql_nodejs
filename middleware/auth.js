9f5b3d16e474dcb6add4f974cc3fd7098a696612
Pokem <pokempatrick@yahoo.fr>
2023-05-25 12:00:16 +0100

    simple graphql app with authentification

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,26 +4,34 @@ module.exports = async (req, res, next) => {
     try {
         if (
             !(
-                req.body.query.includes("login") ||
-                req.body.query.includes("sendCode") ||
-                req.body.query.includes("checkCode")
+                req.body.query?.includes("login") ||
+                req.body.query?.includes("sendCode") ||
+                req.body.query?.includes("checkCode")
             )
         ) {
-            const token = req.headers.authorization.split(" ")[1];
-            const decodedToken = jwt.verify(
-                token,
-                process.env.SECRET_TOKEN || "RANDOM_TOKEN_SECRET"
-            );
-            const { _id, email, firstName, lastName, role, limitedAccessDate } =
-                await User.findOne({ _id: decodedToken.userId });
-            req.user = {
-                _id,
-                email,
-                firstName,
-                lastName,
-                role,
-                limitedAccessDate,
-            };
+            const token = req.headers.authorization?.split(" ")[1];
+            if (token) {
+                const decodedToken = jwt.verify(
+                    token,
+                    process.env.SECRET_TOKEN || "RANDOM_TOKEN_SECRET"
+                );
+                const {
+                    _id,
+                    email,
+                    firstName,
+                    lastName,
+                    role,
+                    limitedAccessDate,
+                } = await User.findOne({ _id: decodedToken.userId });
+                req.user = {
+                    _id,
+                    email,
+                    firstName,
+                    lastName,
+                    role,
+                    limitedAccessDate,
+                };
+            }
         }
         next();
     } catch (error) {
